da2c7ba12b91797a70591acd11af5552d4f49f0e
wev2530 <wev049665@gmail.com>
2024-09-23 23:02:27 +0100

    update main.js

--- main.js
+++ main.js
@@ -12,20 +12,28 @@ const tap = document.getElementById("mineBtn");
 
 const stone = "stone";
 
-const iron = "iron"
+const iron = "iron";
 
-const bronze = "bronze"
+const bronze = "bronze";
 
-const silver = "silver"
+const silver = "silver";
 
-const gold = "gold"
+const gold = "gold";
 
-const diamond = "diamond"
+const diamond = "diamond";
 
-const w1 = 
+const platinum = "platinum";
+
+const master = "master";
+
+const grandMaster = "grand master";
+
+const lord = "lord";
+
+const creator = "creator";
 
 let energyCount = 500;
-let mainBalance = 100;
+let mainBalance = 20000000000;
 balance.textContent = mainBalance;
 
 energy.textContent = energyCount;
@@ -40,6 +48,36 @@ tap.addEventListener("click", function () {
   
   console.log(energyCount -5)
   
-  if (balance => 100) {
+  if (mainBalance === 100) {
     leaderboard.textContent = stone;
+  }
+  else if (mainBalance === 1000) {
+    leaderboard.textContent = iron;
+  }
+  else if (mainBalance === 10000) {
+    leaderboard.textContent = bronze;
+  }
+  else if (mainBalance === 100000) {
+    leaderboard.textContent = silver;
+  }
+  else if (mainBalance === 1000000) {
+    leaderboard.textContent = gold;
+  }
+  else if (mainBalance === 5000000) {
+    leaderboard.textContent = diamond;
+  }
+  else if (mainBalance === 10000000) {
+    leaderboard.textContent = platinum;
+  }
+  else if (mainBalance === 50000000) {
+    leaderboard.textContent = master;
+  }
+  else if (mainBalance === 100000000) {
+    leaderboard.textContent = grandMaster;
+  }
+  else if (mainBalance === 1000000000) {
+    leaderboard.textContent = lord;
+  }
+  else if (mainBalance =>20000000000) {
+    leaderboard.textContent = creator;
   }
