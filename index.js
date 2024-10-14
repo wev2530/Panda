dbd35a4ea5f9a0421b3bd54821869f28260ea7e9
wev2530 <wev049665@gmail.com>
2024-10-14 10:49:34 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const frenEl = document.querySelectorAll(".fren");
+const frenEl = document.querySelector(".fren");
+
+const friendsPage = document.getElementById("friendsPage")
+
+const mainPage = document.getElementById("mainPage")
+
+frenEl.addEventListener('click', function() {
+
+  var stylesheet = document.getElementById('stylesheet');
+
+  // Change the href attribute to point to a different CSS file
+  stylesheet.href = "/friends.css";
+  friendsPage.classList.remove("hidden")
+  mainPage.classList.add("hidden")
+})
+;
+
+mineEl.addEventListener('click', function() {
+
+
+
+  var stylesheet = document.getElementById('stylesheet');
+
+  // Change the href attribute to point to a different CSS file
+  stylesheet.href = "/index.css";
+  friendsPage.classList.add("hidden")
+  mainPage.classList.remove("hidden")
+})
+;
 const mineEl = document.querySelectorAll(".mineEl");
 const taskEl = document.querySelectorAll(".taskEl");
 const aDrop = document.querySelectorAll(".aDrop");
 
-frenEl.addEventListener("click", function(){
-    
-})
+// JavaScript to handle click event and change the CSS file
