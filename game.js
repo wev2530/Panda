08b6c3c3922e7cb6593bbd421172822b40cc1326
wev2530 <wev049665@gmail.com>
2024-10-30 22:49:55 +0100

    modified:   index.css

---
+++ game.js
@@ -0,0 +1,8 @@
+const frenPg = document.querySelector(".frenEl");
+const body = document.querySelector("body");
+
+frenPg.addEventListener("click", function(){
+    body.classList.add("frenPg");
+    console.log("ha ha fren was clicked");
+    
+})
