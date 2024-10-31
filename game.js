c22b80dad86ea907af192b37d613b958b209d567
wev2530 <wev049665@gmail.com>
2024-10-31 11:48:51 +0100

    update

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 const frenPg = document.querySelector(".frenEl");
 const body = document.querySelector("body");
+const firstFooter = document.querySelector(".footer0")
+const boostBtn = document.querySelector(".boost")
 
 frenPg.addEventListener("click", function(){
     body.classList.add("frenPg");
     console.log("ha ha fren was clicked");
-    
+    firstFooter.classList.add("none")
+    boostBtn.classList.add("none")
 })
